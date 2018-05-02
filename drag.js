d7d6b2020773c0ffb410ca6d9f90babcc9eac1be
王栋 <wangdong9603@163.com>
2018-05-02 12:36:02 +0800

    表格排序及拖拉列表

---
+++ drag.js
@@ -0,0 +1,66 @@
+Vue.directive('drag', {
+  inserted (el, binding) {
+    el.style.boxSizing = 'border-box';
+    const dragCells = Array.from(el.tHead.rows[0].cells);
+    let margin = 10;
+    if(binding.value) {
+      margin = binding.value;
+    }
+    let changeCellWidth;
+    const positionMark = {
+      clientX: '',
+      clientOldWidth: '',
+      clientWidth: '',
+      mousedown: false,
+      target: '',
+      positionTh: 0,
+    }
+    for (const [index,th] of dragCells.entries()) {
+      if (th !== dragCells[dragCells.length - 1]) {
+        th.addEventListener('mousedown', function(e) {
+          if (e.offsetX > e.target.clientWidth - margin) {
+            for (const setThWidth of dragCells) {
+              if(setThWidth !== dragCells[index + 1]) {
+                setThWidth.width = setThWidth.offsetWidth; // 设置除下一节点外的节点宽度
+              }
+            }
+            dragCells[index + 1].width = '';  // 移除下一节点宽度
+            positionMark.clientX = e.clientX;
+            positionMark.mousedown = true;
+            positionMark.clientOldWidth = e.target.clientWidth;
+            positionMark.target = th;
+            positionMark.positionTh = index + 1;
+          }
+        });
+      }
+      th.addEventListener('mousemove', function(e) {
+        if (e.offsetX > th.clientWidth - margin && th !== dragCells[dragCells.length - 1]) {
+          th.style.cursor = 'col-resize';
+        } else {
+          th.style.cursor = '';
+        };
+      });
+    }
+    document.addEventListener('mouseup', function (e) {
+      if(positionMark.mousedown) {
+        dragCells[positionMark.positionTh].width = dragCells[positionMark.positionTh].clientWidth; // 设置下一节点的节点宽度
+      };
+      positionMark.mousedown = false;
+    })
+    el.addEventListener('mousemove', function (e) {
+      if (positionMark.mousedown === true) {
+        if (positionMark.clientOldWidth + (e.clientX - positionMark.clientX) > 0) {
+          if (e.target === positionMark.target) {    //在本元素上拉长宽度
+              e.target.style.cursor = 'col-resize';
+              positionMark.clientWidth = positionMark.clientOldWidth + (e.clientX - positionMark.clientX);
+              e.target.width = positionMark.clientWidth;
+          } else {
+            e.target.style.cursor = '';
+            positionMark.clientWidth = positionMark.clientOldWidth + (e.clientX - positionMark.clientX);
+            positionMark.target.width = positionMark.clientWidth;
+          };
+        };
+      };
+    });
+  }
+})
---
+++ sort.js
@@ -0,0 +1,67 @@
+Vue.directive('sort', {
+  inserted (el, binding, vnode) {
+    const sortCells = el.tHead.rows[0].cells;
+    let sortActive = '';
+    /**@type {boolean}*/
+    let isRight = false;
+    const mark = {
+      nameMark: binding.value[0],
+      sortMark: binding.value[1],
+    };
+    for (const cell of sortCells) {
+      if (cell.hasAttribute('sort-field')) {
+        if (mark.nameMark === cell.getAttribute('sort-field')) {
+          isRight = true;
+        }
+        cell.innerHTML = `<span>${cell.innerHTML} <i class='fa fa-sort'></i></span>`
+        const sortArrow = cell.querySelector('.fa-sort')
+        sortArrow.style.cursor = 'pointer';
+        sortArrow.setAttribute('aria-hidden', 'true');
+        const spanDom = sortArrow.closest('span')
+          spanDom.style.cursor = 'pointer';
+          spanDom.addEventListener('click', function(e) {
+          mark.nameMark = cell.getAttribute('sort-field');
+          if (sortActive === '' || sortActive === cell.getAttribute('sort-field')) {    //点击本节点时
+            sortActive = cell.getAttribute('sort-field');
+            let changeClass = /fa-sort-(.*)/.exec(sortArrow.className);
+            switch (changeClass ? changeClass[1] : 'default') {
+              case 'asc':
+                sortArrow.classList.remove('fa-sort-asc');
+                sortArrow.classList.add('fa-sort');
+                mark.sortMark = 'default';
+                break;
+              case 'desc':
+                sortArrow.classList.remove('fa-sort-desc');
+                sortArrow.classList.add('fa-sort-asc');
+                mark.sortMark = 'asc';
+                break;
+              case 'default':
+                sortArrow.classList.remove('fa-sort');
+                sortArrow.classList.add('fa-sort-desc');
+                mark.sortMark = 'desc';
+                break;
+            }
+          } else {                                              //点击其他元素时
+            for (const dom of Array.from(spanDom.closest('tr').children)) {
+              if(dom.getAttribute('sort-field') === sortActive) {
+                sortArrow.classList.remove('fa-sort');
+                sortArrow.classList.add('fa-sort-desc');
+                dom.childNodes[0].childNodes[1].classList.remove('fa-sort-desc');
+                dom.childNodes[0].childNodes[1].classList.remove('fa-sort-asc');
+                dom.childNodes[0].childNodes[1].classList.add('fa-sort');
+                mark.sortMark = 'desc';
+              }
+            }
+            sortActive = cell.getAttribute('sort-field')
+          }
+          vnode.elm.dispatchEvent(new CustomEvent('sort', {detail: mark}));
+        })
+      }
+    }
+    if (isRight) {
+      vnode.elm.dispatchEvent(new CustomEvent('sort', {detail: '初始化排序成功'}));
+    } else {
+      throw new Error("初始化排序失败，检查拼写或查看是否绑定'sort-field'");
+    }
+  }
+})
